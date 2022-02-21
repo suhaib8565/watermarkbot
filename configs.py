a2528868753939c181a7a73f0f2e9ec2b23f20ce
suhaib8565 <91482773+suhaib8565@users.noreply.github.com>
2022-02-21 10:37:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -31,7 +31,7 @@ Hi, I am Video Watermark Adder Bot!
 
 __Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/Dsrs_Group).__
 
-Desgined by @Dsrs_Group
+Desgined by @kashir_bots
 """
 	PROGRESS = """
 Percentage : {0}%
